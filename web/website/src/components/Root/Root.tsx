6f9c503922209e80ceaadc23e44f9804fce36ff7
arenacraftwow <arenacraftwow@gmail.com>
2020-03-08 10:54:00 +0100

    Init website commit

---
+++ web/website/src/components/Root/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Root.css';
+
+export function Root() {
+    return (
+        <div className="Root">
+            <nav className="Root-navigation">
+
+            </nav>  
+        </div>
+    )
+}
